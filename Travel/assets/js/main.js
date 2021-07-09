7524e37fbb4359d561354029bb66bf5135f0d89e
Umarkhon <umarkhonakhmedov2000@gmail.com>
2021-07-09 17:34:43 +0500

    Adding navbar for mobile size

---
+++ Travel/assets/js/main.js
@@ -0,0 +1,27 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+// Show Menu
+if(navToggle){
+  navToggle.addEventListener('click', () =>{
+    navMenu.classList.add('show-menu');
+  })
+}
+
+if(navClose){
+  navClose.addEventListener('click', () =>{
+    navMenu.classList.remove('show-menu');
+  })
+}
+
+// Remove Menu Mobile
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+  const navMenu = document.getElementById('nav-menu');
+  // When we click on each nav__link, we remove the show-menu
+  navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
