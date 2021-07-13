bd005a1e78070293828ff11e4571bb23db7b0742
Umarkhon <umarkhonakhmedov2000@gmail.com>
2021-07-13 07:52:25 +0500

    Adding video section

--- Travel/assets/js/main.js
+++ Travel/assets/js/main.js
@@ -47,6 +47,50 @@ var swiper = new Swiper(".discover__container", {
   spaceBetween: 32,
   coverflowEffect: {
     rotate: 0,
-
   },
 });
+
+// Video
+const videoFile = document.getElementById('video-file'),
+videoButton = document.getElementById('video-button'),
+videoIcon = document.getElementById('video-icon');
+
+function playPause(){
+  if(videoFile.paused){
+    // Play video
+    videoFile.play()
+
+    // We change the icon
+    videoIcon.classList.add('ri-pause-line');
+    videoIcon.classList.remove('ri-play-line');
+  }else {
+    // Pause video
+    videoFile.pause()
+    // We change the icon
+    videoIcon.classList.remove('ri-pause-line');
+    videoIcon.classList.add('ri-play-line');
+  }
+}
+
+videoButton.addEventListener('click', playPause)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
