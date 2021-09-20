f6af566bb58c3b2fe8a982ea9db32e6480249112
UmarkhonAkhmedov <umarkhonakhmedov2000@gmail.com>
2021-09-20 17:39:06 +0500

    Adding about section

--- Another Portfolio/js/app.js
+++ Another Portfolio/js/app.js
@@ -1,4 +1,7 @@
 const filter_btns = document.querySelectorAll(".filter-btn");
+const skills_wrap = document.querySelector(".skills");
+const skills_bars = document.querySelectorAll(".skill-progress");
+
 
 filter_btns.forEach(btn => btn.addEventListener("click", () => {
   filter_btns.forEach(button => button.classList.remove("active"));
@@ -15,3 +18,17 @@ $('.grid').isotope({
   layoutMode: 'fitRows',
   transitionDuration: "0.6s",
 });
+
+window.addEventListener("scroll", () => {
+  checkScroll(skills_wrap);
+});
+function checkScroll(el) {
+  let rect = el.getBoundingClientRect();
+  if(window.innerHeight >= rect.top + el.offsetHeight) return true;
+    return false;
+}
+
+function skillsEffect(){
+  if(!checkScroll(skills_wrap)) return;
+  skills_bars.forEach((skill => skill.style.width = skill.dataset.progress));
+}
